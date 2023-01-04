bd8087506d78b84bb4b6e41a2f6290dd47b432b5
Jiyong Youn <01@0101010101.com>
2023-01-04 20:07:51 +0900

    Bypass nonce checking

--- core/state_transition.go
+++ core/state_transition.go
@@ -216,17 +216,18 @@ func (st *StateTransition) preCheck() error {
 	// Only check transactions that are not fake
 	if !st.msg.IsFake() {
 		// Make sure this transaction's nonce is correct.
-		stNonce := st.state.GetNonce(st.msg.From())
-		if msgNonce := st.msg.Nonce(); stNonce < msgNonce {
-			return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooHigh,
-				st.msg.From().Hex(), msgNonce, stNonce)
-		} else if stNonce > msgNonce {
-			return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooLow,
-				st.msg.From().Hex(), msgNonce, stNonce)
-		} else if stNonce+1 < stNonce {
-			return fmt.Errorf("%w: address %v, nonce: %d", ErrNonceMax,
-				st.msg.From().Hex(), stNonce)
-		}
+		// bypass nonce checker (hletrd)
+		//stNonce := st.state.GetNonce(st.msg.From())
+		//if msgNonce := st.msg.Nonce(); stNonce < msgNonce {
+		//	return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooHigh,
+		//		st.msg.From().Hex(), msgNonce, stNonce)
+		//} else if stNonce > msgNonce {
+		//	return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooLow,
+		//		st.msg.From().Hex(), msgNonce, stNonce)
+		//} else if stNonce+1 < stNonce {
+		//	return fmt.Errorf("%w: address %v, nonce: %d", ErrNonceMax,
+		//		st.msg.From().Hex(), stNonce)
+		//}
 		// Make sure the sender is an EOA
 		if codeHash := st.state.GetCodeHash(st.msg.From()); codeHash != emptyCodeHash && codeHash != (common.Hash{}) {
 			return fmt.Errorf("%w: address %v, codehash: %s", ErrSenderNoEOA,
